b7db1dad29a22c222807a41686ed306adf9ba5f3
JuYoung <ken44929@gmail.com>
2021-09-11 15:05:56 +0900

    Style: #16 Fix toggle style

--- client/src/components/common/Toggle/ToggleStyle.js
+++ client/src/components/common/Toggle/ToggleStyle.js
@@ -3,25 +3,25 @@ import styled from 'styled-components';
 export const ToggleContainer = styled.div`
   display: inline-flex;
   align-items: center;
-  justify-content: center;
-  height: 100px;
-  width: 300px;
-  background: #dde1e7;
+
+  background: #ecf0f3;
   border-radius: 5px;
   box-shadow: -3px -3px 7px #ffffff73, 3px 3px 5px rgba(94, 104, 121, 0.288);
-  margin-right: 40px;
+
+  min-height: 2.5rem;
+  padding: 0.625rem;
+  border-radius: 0.25rem;
 `;
 
 export const ToggleBox = styled.div`
-  height: 40px;
-  width: 100px;
+  height: 20px;
+  width: 40px;
 
   & > input {
     position: relative;
     cursor: pointer;
     height: 100%;
     width: 100%;
-    background: ${({ onToggle }) => (onToggle ? 'lightblue' : '#dde1e7')};
     outline: none;
     -webkit-appearance: none;
     border-radius: 25px;
@@ -34,8 +34,8 @@ export const ToggleBox = styled.div`
     content: '';
     left: 0;
     top: 0;
-    width: 40px;
-    height: 40px;
+    width: 20px;
+    height: 20px;
     background: #dde1e7;
     transition: left 0.2s ease;
 
@@ -44,13 +44,11 @@ export const ToggleBox = styled.div`
   }
 
   & > input:checked:before {
-    left: 60px;
+    left: 20px;
     box-shadow: -8px -4px 8px 0px #ffffff73;
   }
 `;
 
 export const ToggleText = styled.div`
-  margin-left: 20px;
-  font-size: 20px;
   letter-spacing: 0.2px;
 `;
