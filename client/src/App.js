fa4bf28498ce5b7996df7cfa26f6b480bc4a7509
JuYoung <ken44929@gmail.com>
2021-09-11 14:59:34 +0900

    Feat: #16 Add PostPage route

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Main from 'pages/Main/Main';
 import Login from 'pages/Login/Login';
 import SignUp from 'pages/SignUp/SignUp';
 import Navbar from 'components/Navbar/Navbar';
+import Post from 'pages/Post/Post';
+import Test from 'pages/Test/Test';
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <Route exact path={ROUTES.MAIN} component={Main} />
       <Route path={ROUTES.SIGNUP} component={SignUp} />
       <Route path={ROUTES.LOGIN} component={Login} />
+      <Route path={ROUTES.POST} component={Post} />
+      <Route path={ROUTES.TEST} component={Test} />
     </div>
   );
 };
