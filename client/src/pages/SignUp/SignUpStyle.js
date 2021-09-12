3b384c56498f1e47bdc8491a4866613f538c87a9
JuYoung <ken44929@gmail.com>
2021-09-12 14:29:00 +0900

    Change color and shadow to variable name

--- client/src/pages/SignUp/SignUpStyle.js
+++ client/src/pages/SignUp/SignUpStyle.js
@@ -13,7 +13,7 @@ export const Title = styled.div`
   font-size: 30px;
   font-weight: 600;
   margin-bottom: 25px;
-  color: #595959;
+  color: ${({ theme }) => theme.color.black};
 `;
 
 export const Wrapper = styled.div`
@@ -25,21 +25,12 @@ export const Wrapper = styled.div`
   width: 600px;
   padding: 60px 35px 35px 35px;
   border-radius: 40px;
-  background: #ecf0f3;
+  background: ${({ theme }) => theme.color.background};
   box-shadow: 6px 6px 12px #ceced1, -4px -2px 12px #ffffff;
 
   @media screen and (max-width: 600px) {
     box-shadow: none;
     padding: 10px 20px;
-  }
-
-  & > hr {
-    width: 80%;
-    background-color: #f0f0f0;
-
-    @media screen and (max-width: 600px) {
-      width: 90%;
-    }
   }
 
   & > form {
@@ -62,13 +53,13 @@ export const Field = styled.div`
     width: 100%;
     padding-left: 45px;
     font-size: 16px;
-    color: #595959;
+    color: ${({ theme }) => theme.color.black};
     background: #ecf0f3;
     border-radius: 25px;
     box-shadow: inset 2px 2px 5px #babecc, inset -5px -5px 10px #ffffff73;
   }
   & > input:focus {
-    color: #2d4cc8;
+    color: ${({ theme }) => theme.color.primary};
     box-shadow: inset 1px 1px 2px #babecc, inset -1px -1px 2px #ffffff73;
   }
 
@@ -80,7 +71,7 @@ export const Field = styled.div`
     position: absolute;
     width: 50px;
     line-height: 50px;
-    color: #595959;
+    color: ${({ theme }) => theme.color.black};
   }
 `;
 
@@ -95,7 +86,7 @@ export const Button = styled.button`
   cursor: pointer;
   border-radius: 25px;
   box-shadow: ${({ signUpSuccess, theme }) =>
-    signUpSuccess ? theme.shadow.primary : theme.shadow.hover};
+    signUpSuccess ? theme.shadow.primary : theme.shadow.pressed};
   color: ${({ signUpSuccess, theme }) =>
     signUpSuccess ? theme.color.white : theme.color.lightgray};
   transition: ${({ theme }) => theme.transition.primary};
